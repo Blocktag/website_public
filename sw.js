08a928958434c601487e71761db7dfdc03ff216a
@chunming <chunming@blocktag.com>
2021-10-25 13:58:47 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2744e17b321ec896100b.js"
+    "url": "webpack-runtime-048e0bfc62c86dfcc582.js"
   },
   {
     "url": "framework-02088522f04f6be47b0f.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f767ef046e94fa6d2589ccb3def28446"
+    "revision": "d85ee7edc2a8509f5d7bbf619c34cc1b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
